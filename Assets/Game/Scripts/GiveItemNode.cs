02ea3f93fee676aa851c989bb755c7813dfe0ef6
Cory Leach <cory@pracystudios.com>
2025-01-19 17:49:56 -0600

    Update GiveItemNode.cs

--- Assets/Game/Scripts/GiveItemNode.cs
+++ Assets/Game/Scripts/GiveItemNode.cs
@@ -1,32 +1,33 @@
-using System.Collections;
-using XNode;
-
-public class GiveItemNode : InteractionNode, ICoroutineNode
-{
-    [Input] public InteractionNodeConnection previous;
-    
-    [Output(connectionType:ConnectionType.Override)]
-    public InteractionNodeConnection next;
-
-    public string itemId;
-    
-    public override NodePort GetNextPort(Interactable interactable)
-    {
-        return GetPort(nameof(next));
-    }
-
-    public override NodePort GetPreviousPort()
-    {
-        return GetPort(nameof(previous));
-    }
-
-    public IEnumerator Execute(Interactable interactable)
-    {
-        GameManager.Instance.InventoryItem = itemId;
-        yield return DialogueManager.Instance.RunDialogue(new DialogueOptions()
-        {
-            style = DialogueStyle.NarratorDialogue,
-            text = $"{itemId} Get!"
-        });
-    }
-}
+// using System.Collections;
+// using UnityEngine.Serialization;
+// using XNode;
+//
+// public class GiveItemNode : InteractionNode, ICoroutineNode
+// {
+//     [Input] public InteractionNodeConnection previous;
+//     
+//     [Output(connectionType:ConnectionType.Override)]
+//     public InteractionNodeConnection next;
+//
+//     public string itemId;
+//     
+//     public override NodePort GetNextPort(Interactable interactable)
+//     {
+//         return GetPort(nameof(next));
+//     }
+//
+//     public override NodePort GetPreviousPort()
+//     {
+//         return GetPort(nameof(previous));
+//     }
+//
+//     public IEnumerator Execute(Interactable interactable)
+//     {
+//         GameManager.Instance.InventoryItem = itemId;
+//         yield return DialogueManager.Instance.RunDialogue(new DialogueOptions()
+//         {
+//             style = DialogueStyle.NarratorDialogue,
+//             text = $"{itemId} Get!"
+//         });
+//     }
+// }
